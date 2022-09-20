eae8d5a973b092b9e853971092b1de29f7fe3099
heinrichhaenn <chunxuhan007@gmail.com>
2022-09-20 23:39:36 +0800

    multi-core

--- bm_doublets_dc_server.R
+++ bm_doublets_dc_server.R
@@ -1,10 +1,15 @@
 ## This is a R scripts run on server ##
 ## Library as less as you can and save the output carefully##
 
+print(paste('The number of available cores is',detectCores()))
+cl <- makeCluster(8, type="SOCK")
+
+
+library(parallel)
+library(snow)
 library(Seurat)
 library(stringr)
 library(dplyr)
-library(tidyverse)
 library(CIMseq)
 library(Cairo)
 
@@ -22,6 +27,59 @@ connection_list <- list()
 de_result_list <- list()
 mult.pred_list <- list()
 
+
+doublets_dec <- function(sample_id){
+  result <- list()
+  bm_sub <- subset(bm_origin,orig.ident == sample_id)
+  singlets <- singlets_list[str_detect(singlets_list,sample_id)]
+  doublets <- doublets_list[str_detect(doublets_list,sample_id)]
+  raw_counts <- as.data.frame(bm_sub@assays$RNA@counts,stringsAsFactors = F)
+  counts.sng <- select(raw_counts,singlets)[all.genes,]
+  counts.sng[is.na(counts.sng)] <- 0
+  counts.sng <- as.matrix(counts.sng)
+  counts.sng[1:2, 1:2]
+  counts.mul <- select(raw_counts,doublets)[all.genes,]
+  counts.mul[is.na(counts.mul)] <- 0
+  counts.mul <- as.matrix(counts.mul)
+  counts.mul[1:2, 1:2]
+  
+  bm_singlets <- subset(bm,orig.ident == sample_id)
+  classes <- as.character(as.data.frame(Idents(bm_singlets))$`Idents(bm_singlets)`)
+  dim <- Embeddings(object = bm_singlets, reduction = "pca")
+  features <- match(VariableFeatures(bm_singlets),rownames(bm_singlets))
+  
+  print(paste('Now the deconvolution is',sample_id,'is ready!'))
+  cObjSng.si <-CIMseqSinglets(counts=counts.sng, classification=classes, norm.to=10000)
+  cObjMul.si <- CIMseqMultiplets(counts=counts.mul, features=features,norm.to=10000)
+  
+  sObj.si <- CIMseqSwarm(cObjSng.si, cObjMul.si, maxiter=100, swarmsize=110, nSyntheticMultiplets=200, seed=123)
+  result[[1]] <- sObj.si
+  deconvolution_result <- sObj.si@fractions
+  result[[2]] <- deconvolution_result
+  
+  si.edges <- calculateEdgeStats(sObj.si, cObjSng.si, cObjMul.si, multiplet.factor=2, maxCellsPerMultiplet=3)
+  print(paste('The top ten connection within',sample_id,'is as below:'))
+  result[[3]] <- si.edges %>% filter(pval < 5e-2 & weight > 3) %>% arrange(desc(score)) %>% head(n=10)
+  print('The circos plot is ready!')
+  Cairo::CairoPNG(filename = paste(sample_id,'.png',sep = ''),
+                  width = 7,
+                  height = 7,
+                  units = "in",
+                  dpi = 300
+  )
+  plotSwarmCircos(sObj.si, cObjSng.si, cObjMul.si, weightCut=3,
+                  maxCellsPerMultiplet=3, alpha=0.05, h.ratio=0.3,
+                  depleted=F, multiplet.factor=2,legend=F
+  )
+  dev.off()
+  print(paste('The circos plot of',sample_id,'is stored in',paste(sample_id,'.png',sep = ''),sep = ' '))
+  print(paste('The deconvolution of',sample_id,'is done!',sep = ' '))
+  mult.pred <- adjustFractions(singlets=cObjSng.si, multiplets=cObjMul.si, swarm=sObj.si, binary=T, maxCellsPerMultiplet=3,multiplet.factor = 2)
+  result[[4]] <- mult.pred
+  file_name <- paste('./',sample_id,'_result.rds',sep='')
+  saveRDS(result,file_name)
+  return(result)
+}
 for (i in 1:length(samples)){
   sample_id <- samples[i]
   bm_sub <- subset(bm_origin,orig.ident == sample_id)
@@ -75,3 +133,5 @@ for (i in 1:length(samples)){
 saveRDS(sObj.si_list,'./output/sObj.si_list.rds')
 saveRDS(connection_list,'./output/connection_list.rds')
 saveRDS(de_result_list,'./output/de_result_list.rds')
+
+stopCluster()
