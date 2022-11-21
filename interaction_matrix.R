2d3ace110ccb957caa37acb6fdef324062379f8a
heinrichhaenn <chunxuhan007@gmail.com>
2022-11-21 17:10:30 +0800

    done

--- interaction_matrix.R
+++ interaction_matrix.R
@@ -3,6 +3,7 @@ library(psych)
 library(dplyr)
 ## Interaction Matrix ##
 setwd('~/Desktop/doublet/bm/Doublets/')
+
 interactions_csv <- read.csv('~/Desktop/doublet/wiring/Leuk_interaction_affinities.csv',header = TRUE)
 
 ## Define a function to calculate the average expression of each cell type
@@ -21,9 +22,13 @@ write.csv(bm_avg_expr, file = "./bm_avg_expr.csv")
 cal_gm_mean <- function(gene_list,cell){
   expr <- c()
   for (i in 1:length(gene_list)){
-    expr <- c(expr,bm_avg_expr[gene_list[i],cell])
+    if (gene_list[i] %in% rownames(bm_avg_expr)){
+      expr <- c(expr,bm_avg_expr[gene_list[i],cell])
+    } else {
+      expr <- c(expr,0)
+    }
   }
-  gm_mean <- geometric.mean(expr)
+  gm_mean <- geometric.mean(expr,na.rm = TRUE)
   return(gm_mean)
 }
 
@@ -35,15 +40,35 @@ cal_expr_score <- function(ligand,receptor,transmitter,receiver,method){
   if (method == 'counts'){
     expr_score <- l_expr * r_expr
     return(expr_score)
-  }
-  else if (method == 'Kd'){
-    Kd_nM <- filter(interactions_csv,gene_L == ligand,gene_R == receptor)$Kd_nM
+  } else if (method == 'Kd'){
+    Kd_nM <- filter(interactions_csv,Gene_L == ligand,Gene_R == receptor)$Kd_nM
     expr_score <- (l_expr * r_expr) / Kd_nM
     return(expr_score)
   }
 }
 
 
-cal_intxn_score <- function(transmitter_expr,receiver_expr,ligand,receptor,method){
+cal_intxn_score <- function(lr_pairs,cell_m,cell_n,method){
+  expr_score <- c()
+  for (i in 1:nrow(lr_pairs)){
+    print(paste('Hi',i))
+    ligand <- lr_pairs[i,]$Gene_L
+    receptor <- lr_pairs[i,]$Gene_R
+    # if cell_m is the transmitter while cell_n is the receiver
+    expr_score_1 <- cal_expr_score(ligand,receptor,cell_m,cell_n,method)
+    # else if cell_m is the receiver while cell_n is the transmitter
+    expr_score_2 <- cal_expr_score(ligand,receptor,cell_n,cell_m,method)
+    expr_score <-c(expr_score,expr_score_1 + expr_score_2)
+  }
+  intxn_score <- sum(expr_score)
+  return(intxn_score)
+}
 
+intxn_mtx <- matrix(0,nrow = 16,ncol = 16,dimnames = list(sort(levels(bm)),sort(levels(bm))))
+for (i in 1:nrow(intxn_mtx)){
+  for(j in 1:ncol(intxn_mtx)){
+    intxn_score <- cal_intxn_score(interactions_csv,cell_m = rownames(intxn_mtx)[i],cell_n = colnames(intxn_mtx)[j],method = 'counts')
+    doublets_name <- paste(sort(c(rownames(intxn_mtx)[i],colnames(intxn_mtx)[j])),collapse = '_')
+    intxn_mtx[i,j] <- intxn_score
+  }
 }
